abafc1bf274adeee6ee4a6093329737370d1b696
Caique de Castro <caique.castro@venturus.org.br>
2023-01-14 19:45:07 -0300

    build: Setup sailboat ui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      // Set font family
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      // Set theme colors (Required config!)
+      colors: {
+        primary: colors.blue,
+        secondary: colors.slate,
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 }
