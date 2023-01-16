0b56371ddb889b6d029bd925a90ced323fd79d44
Caique de Castro <caique.castro@venturus.org.br>
2023-01-16 07:48:02 -0300

    fix: Adjust links on navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,22 +5,31 @@
 <nav class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-primary-900 mb-3">
   <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
     <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
-      <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" href="/">
+      <a
+        class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+        href="/"
+      >
         Shopping List
       </a>
     </div>
     <div class="lg:flex flex-grow items-center" id="example-navbar-warning">
       <ul class="flex flex-col lg:flex-row list-none ml-auto">
-          <li class="nav-item">
-            <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="/items">
-              Para comprar
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
-              Todos itens
-            </a>
-          </li>
+        <li class="nav-item">
+          <a
+            class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+            href="/inventory"
+          >
+            Inventário
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+            href="/items"
+          >
+            Todos itens
+          </a>
+        </li>
       </ul>
     </div>
   </div>
