6a07c0ca13d22e07086481e279871e082e0357f1
Caique de Castro <caique.castro@venturus.org.br>
2023-01-18 11:12:05 -0300

    feat: Add tabs on inventory page

--- src/routes/inventory/+page.svelte
+++ src/routes/inventory/+page.svelte
@@ -8,33 +8,56 @@
   <a href="/inventory/create" class="button">Novo Item do Inventário</a>
 </div>
 
-<table class="w-full border-collapse bg-white text-left text-sm text-gray-500">
-  <thead class="bg-gray-50">
-    <tr>
-      <th scope="col" class="px-6 py-4 font-medium text-gray-900">Item</th>
-      <th scope="col" class="px-6 py-4 font-medium text-gray-900">Quantidade</th>
-      <th scope="col" class="px-6 py-4 font-medium text-gray-900" />
-    </tr>
-  </thead>
-  <tbody class="divide-y divide-gray-100 border-t border-gray-100">
-    {#each data.rows as inventory}
-      <tr>
-        <th class="px-6 py-4 font-medium text-gray-900">{inventory.item?.name}</th>
-        <td class="px-6 py-4">
-          <form action="/inventory/{inventory.id}?/update" method="post" class="inline">
-            <button name="quantity" value="{inventory.quantity - 1}">-</button>
-          </form>
-          {inventory.quantity}
-          <form action="/inventory/{inventory.id}?/update" method="post" class="inline">
-            <button name="quantity" value="{inventory.quantity + 1}">+</button>
-          </form>
-        </td>
-        <td class="flex justify-end gap-4 px-6 py-4 font-medium">
-          <form action="/inventory/{inventory.id}?/delete" method="post">
-            <button>Delete</button>
-          </form>
-        </td>
-      </tr>
-    {/each}
-  </tbody>
-</table>
+<div>
+  <div class="border-b border-b-gray-100">
+    <ul class="-mb-px flex items-center gap-4 text-sm font-medium">
+      <li>
+        <a
+          href="/inventory?filter=toBuy"
+          class="inline-flex cursor-pointer items-center gap-2 px-1 py-3 hover:text-primary-700 relative after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-full {data.filter === '' ? 'text-gray-500' : 'text-primary-700 after:bg-primary-700'} ">
+          Em falta
+        </a>
+      </li>
+      <li>
+        <a
+          href="/inventory"
+          class="relative after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-full inline-flex cursor-pointer items-center gap-2 px-1 py-3 hover:text-primary-700 {data.filter === 'toBuy' ? 'text-gray-500' : 'text-primary-700 after:bg-primary-700'} "
+        >
+          Todos
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div class="py-3">
+    <table class="w-full border-collapse bg-white text-left text-sm text-gray-500">
+      <thead class="bg-gray-50">
+        <tr>
+          <th scope="col" class="px-6 py-4 font-medium text-gray-900">Item</th>
+          <th scope="col" class="px-6 py-4 font-medium text-gray-900">Quantidade</th>
+          <th scope="col" class="px-6 py-4 font-medium text-gray-900" />
+        </tr>
+      </thead>
+      <tbody class="divide-y divide-gray-100 border-t border-gray-100">
+        {#each data.rows as inventory}
+          <tr>
+            <th class="px-6 py-4 font-medium text-gray-900">{inventory.item?.name}</th>
+            <td class="px-6 py-4">
+              <form action="/inventory/{inventory.id}?/update" method="post" class="inline">
+                <button name="quantity" value={inventory.quantity - 1}>-</button>
+              </form>
+              {inventory.quantity}
+              <form action="/inventory/{inventory.id}?/update" method="post" class="inline">
+                <button name="quantity" value={inventory.quantity + 1}>+</button>
+              </form>
+            </td>
+            <td class="flex justify-end gap-4 px-6 py-4 font-medium">
+              <form action="/inventory/{inventory.id}?/delete" method="post">
+                <button>Delete</button>
+              </form>
+            </td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
+</div>
--- src/routes/inventory/+page.ts
+++ src/routes/inventory/+page.ts
@@ -2,10 +2,19 @@ import { supabase } from '$lib/db';
 import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
-export async function load() {
-  const result = await supabase.from('inventory')
+export async function load({ url }) {
+  const filter = url.searchParams.get('filter') ?? '';
+
+  const query = supabase
+    .from('inventory')
     .select('id, quantity, item:items(name)')
     .order('quantity');
+
+  if (filter === 'toBuy') {
+    query.eq('quantity', 0);
+  }
+
+  const result = await query;
 
   if (result.error) {
     throw error(result.status, result.error.message);
@@ -13,5 +22,6 @@ export async function load() {
 
   return {
     rows: result.data ?? [],
+    filter,
   };
 }
