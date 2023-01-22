bd3aa1a5d43130b82fb650e7aa536545f1ea9816
Caique de Castro <caique.castro@venturus.org.br>
2023-01-22 14:37:14 -0300

    style: Fix ident on page to edit item

--- src/routes/items/[id]/edit/+page.svelte
+++ src/routes/items/[id]/edit/+page.svelte
@@ -1,42 +1,42 @@
 <script>
-    /** @type {import('./$types').PageData} */
-    export let data;
-  </script>
+  /** @type {import('./$types').PageData} */
+  export let data;
+</script>
   
-  <div class="flex justify-between mb-4">
-    <h1 class="text-3xl font-semibold mb-2">Editar Item</h1>
-    <a href="/items" class="button">Voltar</a>
-  </div>
-  <div class="max-w-xl mx-auto">
-    <form action="/items/{data.item.id}?/update" method="post" class="space-y-5">
-      <div>
-        <label for="name" class="mb-1 block text-sm font-medium text-gray-700">Item</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          class="input"
-          placeholder="Arroz"
-          value={data.item.name}
-        />
-      </div>
-      <div>
-        <label for="category" class="mb-1 block text-sm font-medium text-gray-700">Categoria</label>
-        <select
-          id="category"
-          name="category"
-          class="input"
-        >
-          {#each data.categories as category}
-            <option
-                value={category.id}
-                selected={data.item.category_id === category.id}
-            >
-                {category.name}
-            </option>
-          {/each}
-        </select>
-      </div>
-      <button class="button">Salvar</button>
-    </form>
-  </div>
+<div class="flex justify-between mb-4">
+  <h1 class="text-3xl font-semibold mb-2">Editar Item</h1>
+  <a href="/items" class="button">Voltar</a>
+</div>
+<div class="max-w-xl mx-auto">
+  <form action="/items/{data.item.id}?/update" method="post" class="space-y-5">
+    <div>
+      <label for="name" class="mb-1 block text-sm font-medium text-gray-700">Item</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        class="input"
+        placeholder="Arroz"
+        value={data.item.name}
+      />
+    </div>
+    <div>
+      <label for="category" class="mb-1 block text-sm font-medium text-gray-700">Categoria</label>
+      <select
+        id="category"
+        name="category"
+        class="input"
+      >
+        {#each data.categories as category}
+          <option
+            value={category.id}
+            selected={data.item.category_id === category.id}
+          >
+            {category.name}
+          </option>
+        {/each}
+      </select>
+    </div>
+    <button class="button">Salvar</button>
+  </form>
+</div>
