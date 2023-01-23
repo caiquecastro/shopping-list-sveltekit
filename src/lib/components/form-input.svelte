f4dcfd96f531b0aaaf0782db6d4c180e76471b4d
Caique de Castro <caique.castro@venturus.org.br>
2023-01-23 09:21:34 -0300

    feat: Add login page

--- src/lib/components/form-input.svelte
+++ src/lib/components/form-input.svelte
@@ -1,21 +1,12 @@
 <script lang="ts">
-    export let id: string;
-    export let label: string;
-    export let value: string | undefined;
-    export let placeholder: string | undefined;
+  export let id: string;
+  export let label: string;
+  export let type = 'text';
+  export let value = '';
+  export let placeholder: string | undefined;
 </script>
 
-<label
-    for={id}
-    class="mb-1 block text-sm font-medium text-gray-700"
->
-    {label}
+<label for={id} class="mb-1 block text-sm font-medium text-gray-700">
+  {label}
 </label>
-<input
-    type="text"
-    id={id}
-    name={id}
-    class="input"
-    placeholder={placeholder}
-    value={value}
-/>
+<input {type} {id} name={id} class="input" {placeholder} {value} />
---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+    import FormInput from "$lib/components/form-input.svelte";
+</script>
+
+<div class="mx-auto max-w-xl">
+  <form action="/auth/login" class="space-y-5" method="post">
+    <div>
+      <FormInput
+        type="email"
+        id="email"
+        label="E-mail"
+        placeholder="you@email.com"
+      />
+    </div>
+    <div>
+      <FormInput
+        id="password"
+        type="password"
+        label="Password"
+        placeholder="Your secret password"
+      />
+    </div>
+    <div class="flex items-center space-x-2">
+      <input
+        type="checkbox"
+        id="remember"
+        class="h-4 w-4 rounded border-gray-300 text-primary-600 shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 focus:ring-offset-0 disabled:cursor-not-allowed disabled:text-gray-400"
+      />
+      <label for="remember" class="text-sm font-medium text-gray-700">Remember me</label>
+    </div>
+    <button type="button" class="button">Entrar</button>
+  </form>
+</div>
