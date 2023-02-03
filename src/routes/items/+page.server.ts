526e40b6f590dce36f981950e9fe1bb95e984447
Caique de Castro <caique.castro@venturus.org.br>
2023-02-03 11:26:23 -0300

    feat: Add logout endpoint

--- src/routes/items/+page.server.ts
+++ src/routes/items/+page.server.ts
@@ -1,8 +1,8 @@
 import { supabase } from '$lib/db';
 import { fail } from '@sveltejs/kit';
+import type { Actions } from './$types';
 
-/** @type {import('./$types').Actions} */
-export const actions = {
+export const actions: Actions = {
   default: async ({ request }) => {
     const formData = await request.formData();
     const name = formData.get('name');
