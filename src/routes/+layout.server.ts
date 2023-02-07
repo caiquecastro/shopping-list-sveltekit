484163fab549b7b2e397a2e994ea85718a0b6050
Caique de Castro <caique.castro@venturus.org.br>
2023-02-07 14:52:55 -0300

    fix: Allow go to signup page if not authenticated

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,11 +2,15 @@ import type { LayoutServerLoad } from './$types';
 import { getServerSession } from '@supabase/auth-helpers-sveltekit';
 import { redirect } from '@sveltejs/kit';
 
-/** @type {import('./$types').LayoutServerLoad} */
+const authRoutes = [
+  '/auth/login',
+  '/auth/register',
+];
+
 export const load: LayoutServerLoad = async (event) => {
   const session = await getServerSession(event);
 
-  if (!session && event.url.pathname !== '/auth/login') {
+  if (!session && !authRoutes.includes(event.url.pathname)) {
     throw redirect(307, '/auth/login');
   }
 
